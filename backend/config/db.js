fb22858d233f576898da6bc72ed3399faddb6708
GajananDhangude <dhangudegajanan27@gmail.com>
2026-02-09 09:46:19 +0530

    database integration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 
-
 main().then(() =>{
     console.log("Connected to the Database")
 }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/learning-assistent');
+  await mongoose.connect('mongodb://127.0.0.1:27017/LearningAssistant');
 
  
 }

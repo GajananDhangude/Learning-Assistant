1fb5e9d07b899a0454f7a8124fd3f220969560ee
GajananDhangude <dhangudegajanan27@gmail.com>
2026-02-09 09:45:35 +0530

    authentication logic

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -31,9 +31,59 @@ async function RegisterUser(req , res) {
     res.status(201).json({
         message:"User registered successfully",
         user:{
+            id:newUser._id,
             firstname:newUser.firstname,
             lastname:newUser.lastname,
             email:newUser.email,
         }
     })
+}
+
+
+async function LoginUser(req , res){
+    const {email , password} = req.body;
+
+    const user = await UserModel.findOne({email:email});
+
+    if(!user){
+        return res.status(400).json({message:"Invalid email or password"});
+    }
+
+    const isPasswordvalid = await bcrypt.compare(password , user.password);
+
+    if(!isPasswordvalid){
+        res.status(400).json({
+            message:"Invalid email or password",
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+    } , process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message:"User Logged in Successfully",
+        user:{
+            id:user._id,
+            FirstName:user.firstname,
+            LastName:user.lastname,
+            Email:user.email,
+
+        }
+    })
+}
+
+
+function LogoutUser(req , res){
+    res.clearCookie("token");
+    res.status(201).json({message:"User logged out Succcessfully."})
+}
+
+
+module.exports = {
+    RegisterUser,
+    LoginUser,
+    LogoutUser
 }

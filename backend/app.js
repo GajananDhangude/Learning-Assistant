3828fb3eb5324f221c307df25c6b61f93defda03
GajananDhangude <dhangudegajanan27@gmail.com>
2026-02-09 09:46:30 +0530

    main

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,45 @@
-const express = require("express")
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv")
 dotenv.config();
-const ConnectDb = require("./config/db.js")
-const cors = require('cors')
-const path = require('path')
-const {fileURLToPath}  = require('url')
+const ConnectDB = require('./config/db.js')
+const cors = require("cors")
+const path = require("path")
+const fileURLToPath = require("url")
+const AuthRoutes = require("./routes/auth.routes.js")
 
 
 const app = express();
-ConnectDb()
-app.use(cors())
+ConnectDB()
+
+app.use(cors({
+    origin:"*",
+    methods:["GET" , "POST" , "PUT" , "DELETE"],
+    allowedHeaders:["Content-Type" , "Authorization"],
+    credentials:true
+}))
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 
 
 app.get("/" , (req , res) =>{
     res.send("Hello World")
-})
+});
+
+app.use("/api" , AuthRoutes);
+
+
+
+
+app.use((req , res) =>{
+    res.status(404).json({
+        success:false,
+        error:"Route not found",
+        statusCode:404
+    });
+});
 
 
 app.listen(8080 , () =>{
